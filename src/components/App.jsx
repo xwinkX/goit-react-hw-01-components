65194f7dc4ae897627f0e22a5147edcacfb231bb
xwinkX <opalyuk.e88@ukr.net>
2022-07-24 10:03:11 +0300

    qqqqqqqqqqq

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { Profile } from 'components/Profile/Profile'
 import  Statistics  from 'components/Statistics/Statistics'
 import FriendList from 'components/FriendList/FriendList'
+import TransactionHistory from 'components/TransactionHistory/TransactionHistory'
 
 import user from 'data/user.json'
 import data from 'data/data.json'
 import friends from 'data/friends.json'
+import transactions from 'data/transactions.json'
 
 export const App = () => {
   return (
@@ -29,6 +31,7 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
       <Statistics stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
