a34650c011b358a7232352b43b720f0a0011d4bc
xwinkX <opalyuk.e88@ukr.net>
2022-07-24 08:21:45 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -37,9 +37,9 @@ Profile.propTypes = {
     location: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
     stats: PropTypes.shape({
-    followers: PropTypes.string.isRequired,
-    view: PropTypes.string.isRequired,
-    likes: PropTypes.string.isRequired
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired
     })
 }
 
